9dc36e8e47ecf0741ddbfde80045e45745b9a410
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-16 15:26:46 +0900

    リマインダー1件分の役割を持つクラスを作成した

---
+++ src/main/java/com/example/linebot/value/ReminderItem.java
@@ -0,0 +1,21 @@
+package com.example.linebot.value;
+
+public class ReminderItem {
+
+    private final String userId;
+    private final ReminderSlot slot;
+
+    public ReminderItem(String userId, ReminderSlot slot){
+        this.userId = userId;
+        this.slot = slot;
+    }
+
+    public ReminderSlot getSlot(){
+        return slot;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+
+}
