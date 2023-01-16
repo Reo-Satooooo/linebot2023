749b93eb439da9be09cde41b72980d26458e2cc5
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-01-16 16:11:36 +0900

    サービスとリポジトリをつなぐ部分を作成した

--- src/main/java/com/example/linebot/service/ReminderService.java
+++ src/main/java/com/example/linebot/service/ReminderService.java
@@ -3,12 +3,21 @@ package com.example.linebot.service;
 import com.example.linebot.replier.RemindOn;
 import com.example.linebot.value.ReminderItem;
 import com.example.linebot.value.ReminderSlot;
+import com.example.linebot.repository.ReminderRepository;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReminderService {
+
+    private final ReminderRepository repository;
+
+    @Autowired
+    public ReminderService(ReminderRepository reminderRepository){
+        this.repository = reminderRepository;
+    }
 
     public RemindOn doReplyOfNewItem(MessageEvent<TextMessageContent> event){
         String userId = event.getSource().getUserId();
@@ -16,6 +25,8 @@ public class ReminderService {
         String text = tmc.getText();
         ReminderSlot slot = new ReminderSlot(text);// スロットの抽出
         ReminderItem item = new ReminderItem(userId, slot);// データベース登録用
+
+        repository.insert(item);
 
         return new RemindOn(text);
     }
